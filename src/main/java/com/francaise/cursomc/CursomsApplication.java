30428ebf873c4e2d42b81ee1ab2bb5f7f4e4a556
Rodrigo Soares <rmasoares10@gmal.com>
2021-11-17 10:52:18 -0300

    Projeto criado

---
+++ src/main/java/com/francaise/cursomc/CursomsApplication.java
@@ -0,0 +1,13 @@
+package com.francaise.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/francaise/cursomc/CursomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francaise.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
